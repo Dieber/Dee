151279ed4b0cf803340f10df3b7cfa68188d16e2
Pinkie Pie's Cupcake <a451120068@gmail.com>
2016-03-28 18:50:52 +0800

    IP done

--- app/src/main/java/com/dieber/dee/MainActivity.java
+++ app/src/main/java/com/dieber/dee/MainActivity.java
@@ -1,7 +1,12 @@
 package com.dieber.dee;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TextView twIP = (TextView) findViewById(R.id.textview1);
+        TextView twCPU = (TextView) findViewById(R.id.textview2);
+        TextView twSDK = (TextView) findViewById(R.id.textview3);
+        //获取wifi服务
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        //判断wifi是否开启
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ipAddress = wifiInfo.getIpAddress();
+        String ip = intToIp(ipAddress);
+        twIP.setText(ip);
     }
+
+    private String intToIp(int i) {
+
+        return (i & 0xFF) + "." +
+                ((i >> 8) & 0xFF) + "." +
+                ((i >> 16) & 0xFF) + "." +
+                (i >> 24 & 0xFF);
+    }
+
+
 }
